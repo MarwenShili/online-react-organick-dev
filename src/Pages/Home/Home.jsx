e2740afd1877f1b5667b5676a6bbea4f54f054ea
marwenshili <eshilimarwen9@gmail.com>
2023-08-31 17:04:35 +0100

    integarte products api

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,33 +1,29 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import bannerImg from "../../assets/icons/Banner.svg";
 import "./Home.css";
 import CardProduct from "../../components/CardProduct/CardProduct";
 import { ThemeContext } from "../../contexts/ThemeContext";
+let URL = "https://api5odhra.zitouna.tech/api/products?populate=deep,4";
 
 function Home() {
   const themeValues = useContext(ThemeContext);
-  let products = [
-    {
-      id: "2",
-      name: "Product 1",
-      description: "description of product 1",
-      price: 10,
-      imgUrl: "",
-      review: 4,
-      discountPrice: 3,
-      quantity: 3,
-    },
-    {
-      id: "1",
-      name: "Product 2",
-      description: "description of product 2",
-      price: 20,
-      imgUrl: "",
-      review: 5,
-      discountPrice: 10,
-      quantity: 9,
-    },
-  ];
+  const [products, setProducts] = useState([]);
+
+  function fetchData() {
+    fetch(URL)
+      .then((response) => response.json())
+      .then((res) => setProducts(res.data))
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  console.log(products);
+
   return (
     <div
       className={themeValues.theme === "light" ? "home_page" : "home_page dark"}
@@ -37,6 +33,7 @@ function Home() {
         {products.map((product) => (
           <CardProduct key={product.id} product={product} />
         ))}
+        {products.length === 0 && <h1>Not Data Found</h1>}
       </div>
     </div>
   );
--- src/components/CardProduct/CardProduct.jsx
+++ src/components/CardProduct/CardProduct.jsx
@@ -1,24 +1,61 @@
 import React from "react";
 import "./CardProduct.css";
 import { useNavigate } from "react-router-dom";
+import { Card } from "antd";
+import cartIcon from "../../assets/icons/cart.svg";
+import { Rate } from "antd";
+
+const { Meta } = Card;
+
 function CardProduct({ product }) {
   const navigate = useNavigate();
+  console.log(product);
 
   const HandleNavigate = () => {
     navigate(`/products/${product.id}`, { state: product });
   };
   return (
-    <div className="card" onClick={HandleNavigate}>
-      <img src="" alt="" />
-      <h3>{product.name}</h3>
-      <p>{product.description}</p>
-      <div className="price">
-        <p>
-          Price : <span>{product.price}</span>
-        </p>
+    <Card
+      onClick={HandleNavigate}
+      style={{
+        width: 300,
+      }}
+      cover={
+        <img alt="example" src={product.attributes.image.data.attributes.url} />
+      }
+      className="new_card"
+      title={<HeaderCard category={product.attributes.type} />}
+    >
+      <Meta title={product.attributes.name} />
+      <div className="prices">
+        <div>
+          Price :
+          <span className="before_discount">
+            ${product.attributes.priceBeforeDiscount}
+          </span>{" "}
+          <span className="after_discount">
+            ${product.attributes.priceAfterDiscount}
+          </span>
+        </div>
+        <Rate
+          disabled
+          className="rate"
+          defaultValue={product.attributes.rating}
+        />
       </div>
-    </div>
+    </Card>
   );
 }
 
 export default CardProduct;
+
+const HeaderCard = ({ category }) => {
+  return (
+    <div className="header_card">
+      <p className="category">{category}</p>
+      <span>
+        <img src={cartIcon} alt="" />
+      </span>
+    </div>
+  );
+};
